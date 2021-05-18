94d160aab5b2c60d897625148b4b6cb5f6c926c3
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-05-18 13:03:44 +0200

    Calc improvement

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,19 +2,17 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-	if(command == "add"){
-		return std::to_string(first+second);
-	}else if(command == "subtract"){
-		return std::to_string(first-second);
-	}else if(command == "multiply"){
-		return std::to_string(first*second);
-	}else if(command == "divide"){
-		if(second == 0){
+	if(command == "add")
+		return std::to_string(first + second);
+	if(command == "subtract")
+		return std::to_string(first - second);
+	if(command == "multiply")
+		return std::to_string(first * second);
+	if(command == "divide"){
+		if(second == 0)
 			return "Division by 0";
-		}else{
-			return std::to_string(first/second);
-		}
-	}else{
-		return "Invalid data";
+		
+		return std::to_string(first / second);	
 	}
+	return "Invalid data";
 }
