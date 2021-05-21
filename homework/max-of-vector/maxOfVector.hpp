d04c6b601ed231e899ab9db99ed05f538285be3a
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-05-21 13:59:47 +0200

    Updated2 function

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -8,7 +8,7 @@ int maxOfVector(const std::vector<int>& vec) {
     int max = std::numeric_limits<int>::min();
 
     for(auto const &el : vec) { 
-	max = (max < el) ? el : max;
+	    max = (max < el) ? el : max;
     }
 
     return max;
