78f60b966c463327be93585f6a58a719130f11ab
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-05-21 14:11:20 +0200

    Updated addEven function

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -3,8 +3,12 @@
 
 int addEven(const std::vector<int>& numbers) {
     int sum = 0;
-    for(auto n : numbers)
-	    if(n % 2 == 0) 
+    
+    for(auto n : numbers){
+	    if(n % 2 == 0){ 
 		    sum += n;
+        }
+    }
+
     return sum;
 }
